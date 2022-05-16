a6dfa1259e25da183dbc71795be603cc4a1d2c29
akshayec004 <38517921+akshayec004@users.noreply.github.com>
2022-05-16 15:53:27 +0530

    Update aws-iam-user.sh

--- scripts/aws-iam-user.sh
+++ scripts/aws-iam-user.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 aws iam wait user-exists --user-name $IAM_USERNAME
+echo "SKIP_USER_DELETION='False'" >> $GITHUB_ENV
 if [[ $? == 255 ]]; then
   echo "SKIP_USER_DELETION='True'" >> $GITHUB_ENV
   echo "User $IAM_USERNAME doesn't exist. Available Users are :"
