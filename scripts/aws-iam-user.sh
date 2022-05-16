7e7e442b79684f3091d1305d7acc602c98f99785
akshayec004 <38517921+akshayec004@users.noreply.github.com>
2022-05-16 15:37:21 +0530

    Update aws-iam-user.sh

--- scripts/aws-iam-user.sh
+++ scripts/aws-iam-user.sh
@@ -2,6 +2,8 @@
 
 aws iam wait user-exists --user-name $IAM_USERNAME
 if [[ $? == 255 ]]; then
+  export SKIP_USER_DELETION="True"
   echo "User $IAM_USERNAME doesn't exist. Available Users are :"
   aws iam list-users | jq -r '.Users[].UserName'
+  
 fi
